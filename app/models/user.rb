c4fc69946631c69e7ae1b0cdf865d0cf92078c8f
Your Name <rex.salisbury@gmail.com>
2015-01-26 22:20:30 -0800

    validators added to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,18 @@
 require 'bcrypt'
 
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
+end
+
 class User < ActiveRecord::Base
-  # users.password_hash in the database is a :string
   include BCrypt
+
+  validates :email, presence: true, email: true, uniqueness: true
+  validates :password_hash, presence: true
 
   def password
     @password ||= Password.new(password_hash)
