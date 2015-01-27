3aace7abc9aa029633da52b1474a4635ce1fe121
Your Name <rex.salisbury@gmail.com>
2015-01-26 21:39:40 -0800

    map up on index.erb

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,15 @@
-$(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+$(document).ready(function() {
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+
+  function initialize() {
+    var mapOptions = {
+      center: { lat: -34.397, lng: 150.644},
+      zoom: 8
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  }
 });
+
+
