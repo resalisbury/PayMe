d2024519765fcb2cd9152f4926a0676c0d8d733b
Your Name <rex.salisbury@gmail.com>
2015-01-26 22:22:28 -0800

    add test but need to have nokogiri installed to run tests

--- spec/controllers/index_spec.rb
+++ spec/controllers/index_spec.rb
@@ -1,1 +1,13 @@
-require 'spec_helper'
+require "spec_helper"
+
+describe "Controller" do
+
+  describe "Home Page" do
+
+    it "should be ok" do
+      get "/"
+      expect(last_response).to be_ok
+    end
+
+  end
+end
