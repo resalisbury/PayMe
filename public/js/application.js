86d178618bcee042783b4773f2687129c1bf4b8e
Rex <rex.salisbury@gmail.com>
2015-01-27 17:16:30 -0800

    can now update map to go to a lat and long

--- public/js/application.js
+++ public/js/application.js
@@ -1,15 +1,33 @@
-
 $(document).ready(function() {
-
   google.maps.event.addDomListener(window, 'load', initialize);
-
-  function initialize() {
-    var mapOptions = {
-      center: { lat: -34.397, lng: 150.644},
-      zoom: 8
-    };
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-  }
+  $('#go').on('submit', update)
 });
+
+
+
+function initialize() {
+  var mapOptions = {
+    center: { lat: -34.397, lng: 150.644},
+    zoom: 8
+  };
+  new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  // debugger;
+}
+
+function update() {
+  event.preventDefault();
+  console.log('what up!');
+  var latitude = parseFloat($("#go").find("input[name='latitude']").val())
+  var longitude = parseFloat($("#go").find("input[name='longitude']").val())
+
+  var mapOptions = {
+    center: { lat: latitude, lng: longitude},
+    zoom: 8
+  };
+  debugger;
+  new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+};
+
 
 
