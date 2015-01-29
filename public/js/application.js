a49aac0d88bd0fcb097cc4adc3e78016d1a6e4b8
Rex <rex.salisbury@gmail.com>
2015-01-28 16:32:44 -0800

    map centered on DBC

--- public/js/application.js
+++ public/js/application.js
@@ -7,8 +7,8 @@ $(document).ready(function() {
 
 function initialize() {
   var mapOptions = {
-    center: { lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: { lat: 37.785077, lng: -122.397171},
+    zoom: 15
   };
   new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
   // debugger;
